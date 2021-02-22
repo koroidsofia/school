5d7637d2863bcdacd2f7d21f4d5a404761739c8e
koroidsofia <74060478+koroidsofia@users.noreply.github.com>
2021-02-22 17:51:53 +0200

    Add files via upload

---
+++ lesson32/main.js
@@ -0,0 +1,44 @@
+// let x = "" + 1 + 0
+// console.log(x)
+// let y = true + false
+// console.log(y)
+// let a = 6 / "3"
+// console.log(a)
+// let b = 4 + 5 + "px"
+// console.log(b)
+// let c = "px" + 5 + 4
+// console.log(c)
+// let d = "4" - 2
+// console.log(d)
+// let e = Number(prompt("Введите первое число?"))
+// f = Number(prompt("Введите второе число?"))
+// alert(e + f)
+// console.log(5 > 4)
+// console.log("ананас" > "яблоко")
+// console.log("2" > "12")
+// console.log("Roma" < "Romanuch")
+// let k = prompt("a=")
+// l = prompt("b=")
+// k++
+// k += l
+// alert(a > b)
+// let a = prompt("Какое число")
+// if (a == 10) {
+//     alert("Юля молодец")
+// } else if (a == 7) {
+//     alert("Юля молодец 2")
+// } else { alert("Юля не молодец") }
+// let a = prompt("Ваш возраст")
+// if (a <= 6) {
+//     alert("Дошкольник")
+// } else if (a >= 7, a <= 17) {
+//     alert("Школьник")
+// } else { alert("Взрослый человек") }
+let a = prompt("В каком году был первый сценарий js")
+if (a == 2015) {
+    alert("Верно")
+} else if (a < 2015) {
+    alert("Неверно,правильный ответ больше")
+} else if (a > 2015) {
+    alert("Неверно,правильный ответ меньше")
+}
