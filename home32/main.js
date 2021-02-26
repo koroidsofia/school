bc7febe4fd0c45a0e9164e1cc63ce92c1620b83b
koroidsofia <74060478+koroidsofia@users.noreply.github.com>
2021-02-26 13:03:55 +0200

    Add files via upload

---
+++ home32/main.js
@@ -0,0 +1,42 @@
+let a = prompt("Українська мова та література")
+if (a < 22) {
+    alert("Не склав")
+} else if (a => 22) {
+    alert("Склав ")
+} 
+let b = prompt("Історія України ")
+if (b < 25) {
+    alert("Не склав")
+} else if (b => 25) {
+    alert("Склав ")
+} 
+let c = prompt("Біологія ")
+if (c < 22) {
+    alert("Не склав")
+} else if (c => 22) {
+    alert("Склав ")
+} 
+let d = prompt("Англійська мова ")
+if (d < 17) {
+    alert("Не склав")
+} else if (d => 17) {
+    alert("Склав ")
+} 
+let e = prompt("Німецька мова ")
+if (e < 17) {
+    alert("Не склав")
+} else if (e => 17) {
+    alert("Склав ")
+} 
+let f = prompt("Французька мова ")
+if (f < 17) {
+    alert("Не склав")
+} else if (f => 17) {
+    alert("Склав ")
+} 
+let g = prompt("Іспанська мова ")
+if (g < 17) {
+    alert("Не склав")
+} else if (g => 17) {
+    alert("Склав ")
+} 
